6c70dbb1c60e4115ee25f57dc99575fee35d3bf9
wiktorrudzki <wiktor1rudzki@gmail.com>
2024-03-16 18:51:38 +0100

    add: client side setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/button';
+import { TextInput } from '@/components/input';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <TextInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/button/Button.test.tsx
@@ -0,0 +1,5 @@
+describe('Button component tests', () => {
+  it('should return true for testing puropse :)', () => {
+    expect(true).toBeTruthy();
+  });
+});
