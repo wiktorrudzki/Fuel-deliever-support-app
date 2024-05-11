325df40ba048789f73c46532bec987a7acfb2515
TomChuck <m.tomczyk@gymmaster.io>
2024-05-12 00:29:46 +0200

    Finished Card mobile component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { Button } from '@/components/button';
 import { TextInput } from '@/components/input';
 
 import './App.css';
+import { Card } from './components/card';
 
 function App() {
   return (
     <div>
       <Button />
       <TextInput />
+      <Card />
     </div>
   );
 }
---
+++ client/src/components/card/Card.tsx
@@ -0,0 +1,37 @@
+// project imports
+import addressIcon from './icons/fa6-regular_address-card.svg';
+import mapIcon from './icons/gg_pin.svg';
+import phoneIcon from './icons/ic_baseline-phone.svg';
+import './style.css';
+
+const Card = () => {
+  return (
+    <div className="card-container">
+      <div className="card-title">
+        <p className="title">Stacja nr 1</p>
+      </div>
+      <div className="card-content">
+        <div className="phone-field content-field">
+          <img src={phoneIcon} className="icon" alt="phone_icon" />
+          <p className="phone content-text">tel: +48 000 000 000</p>
+        </div>
+        <div className="address-field content-field">
+          <img src={addressIcon} className="icon" alt="address_icon" />
+          <div className="address-data">
+            <p className="content-text">kierownik: Imię Nazwisko</p>
+            <p className="content-text">ul. Adresowa nr 1,</p>
+            <p className="content-text">00-000 Miasto, Państwo</p>
+          </div>
+        </div>
+        <div className="map-field content-field">
+          <img src={mapIcon} className="map-icon" alt="map_icon" />
+          <a href="/" className="map-link">
+            Zobacz na mapie &gt;&gt;
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
