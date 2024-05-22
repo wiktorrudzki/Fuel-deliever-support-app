ac905e3b752b30acfc1c25c793e08492240112a8
TomChuck <m.tomczyk@gymmaster.io>
2024-05-22 23:39:55 +0200

    Finished login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import { Button } from '@/components/button';
-import { TextInput } from '@/components/input';
-
+// import { Button } from '@/components/button';
+// import { TextInput } from '@/components/input';
 import './App.css';
+import { LoginPage } from './components/loginPage';
 
 function App() {
   return (
-    <div>
-      <Button />
-      <TextInput />
-    </div>
+    // <div>
+    //   <Button />
+    //   <TextInput />
+    // </div>
+    <LoginPage />
   );
 }
 
